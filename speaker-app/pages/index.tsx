2010031998c7474c470c23c74dcf7bfa27523265
jmjf <jamee.mikell@gmail.com>
2022-06-15 02:11:21 +0000

    1.0.3: FEAT: render a card for one speaker

--- speaker-app/pages/index.tsx
+++ speaker-app/pages/index.tsx
@@ -1,5 +1,41 @@
+import { data } from '../SpeakerData';
+
 const IndexPage = () => {
-	return <div>Hello World!!</div>;
+	const { id, bio, first, last, favorite, twitterHandle, company, sessions } = data[0];
+
+	return (
+		<div className="container speakers-list">
+			<div className="row">
+				<div className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12">
+					<div className="card card-height p-4 mt-4">
+						<div className="speaker-img d-flex flex-row justify-content-center align-items-center h300">
+							<img
+								className="contain-fit"
+								src={`/images/speaker-${id}.jpg`}
+								width="300"
+								alt={`image of ${first} ${last}`}
+							/>
+						</div>
+						<div className="speaker-info">
+							<div className="d-flex justify-content-between mb-3">
+								<h3 className="text-truncate w200">
+									{first} {last}
+								</h3>
+							</div>
+							<p>
+								{bio} {company} {twitterHandle} {favorite}
+							</p>
+						</div>
+					</div>
+					<div className="sessionBox card h-250">
+						<span className="session w-100">
+							{sessions[0].title} <strong>Room: {sessions[0].room.name}</strong>
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default IndexPage;
