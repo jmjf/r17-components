6ace3d1cd8ea61f4394810aa52db4dd85bcf5a15
jmjf <jamee.mikell@gmail.com>
2022-06-16 00:46:12 +0000

    2.0.2 - FEAT: add header and toolbar

--- speaker-app/src/pages/index.tsx
+++ speaker-app/src/pages/index.tsx
@@ -1,7 +1,15 @@
+import { Header } from 'components/Header';
+import { SpeakersToolbar } from 'components/SpeakersToolbar';
 import { Speakers } from 'components/Speakers';
 
 const IndexPage = () => {
-	return <Speakers />;
+	return (
+		<div>
+			<Header />
+			<SpeakersToolbar />
+			<Speakers />
+		</div>
+	);
 };
 
 export default IndexPage;
