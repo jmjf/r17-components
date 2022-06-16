b7de6670e8f621d2b6268c11b09b351ff4521671
jmjf <jamee.mikell@gmail.com>
2022-06-16 21:11:58 +0000

    3.0.1 - FEAT: use state to simulate async data loading

--- speaker-app/src/components/SpeakersList.tsx
+++ speaker-app/src/components/SpeakersList.tsx
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+import ContentLoader from 'react-content-loader';
 
 import { data } from '../SpeakerData';
 
@@ -9,8 +11,40 @@ interface ISpeakersListProps {
 	showSessionsFlag: boolean;
 }
 
+const SpeakerLoader = () => {
+	return (
+		<ContentLoader viewBox="0 0 380 70">
+			<circle cx="30" cy="30" r="30" />
+			<rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
+			<rect x="80" y="40" rx="3" ry="3" width="250" height="10" />
+		</ContentLoader>
+	);
+};
+
 export const SpeakersList = ({ showSessionsFlag }: ISpeakersListProps) => {
-	const [speakers, setSpeakers] = useState(data);
+	const [speakers, setSpeakers] = useState([] as ISpeaker[]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [isLoadError, setIsLoadError] = useState(false);
+	const [loadErrorMessage, setLoadErrorMessage] = useState('');
+
+	const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+	useEffect(() => {
+		const delayedAction = async () => {
+			try {
+				await delay(2000);
+				// throw Error('test error');
+				setIsLoading(false);
+				setSpeakers(data);
+			} catch (e) {
+				const err = e as Error;
+				setIsLoading(false);
+				setIsLoadError(true);
+				setLoadErrorMessage(err.toString());
+			}
+		};
+		delayedAction();
+	}, []);
 
 	const onFavoriteToggle = (speakerId: string) => {
 		const oldSpeaker = speakers.find((speaker: ISpeaker) => speaker.speakerId === speakerId);
@@ -21,20 +55,37 @@ export const SpeakersList = ({ showSessionsFlag }: ISpeakersListProps) => {
 		setSpeakers(newSpeakers);
 	};
 
+	// if (isLoading === true) return <div>Loading...</div>;
+
+	if (isLoadError === true) {
+		return (
+			<div className="text-danger">
+				<p>
+					ERROR: <b>Loading Speaker data failed.</b>
+				</p>
+				<p>{loadErrorMessage}</p>
+			</div>
+		);
+	}
+
 	return (
 		<div className="container speakers-list">
-			<div className="row">
-				{speakers.map((speaker) => {
-					return (
-						<SpeakerCard
-							key={speaker.speakerId}
-							speaker={speaker}
-							onFavoriteToggle={() => onFavoriteToggle(speaker.speakerId)}
-							showSessionsFlag={showSessionsFlag}
-						/>
-					);
-				})}
-			</div>
+			{isLoading ? (
+				<SpeakerLoader />
+			) : (
+				<div className="row">
+					{speakers.map((speaker) => {
+						return (
+							<SpeakerCard
+								key={speaker.speakerId}
+								speaker={speaker}
+								onFavoriteToggle={() => onFavoriteToggle(speaker.speakerId)}
+								showSessionsFlag={showSessionsFlag}
+							/>
+						);
+					})}
+				</div>
+			)}
 		</div>
 	);
 };
